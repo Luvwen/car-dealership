999189566d52a09fd8db9e94e29765b11b81f58f
Santiago Baliño <santiagobalino@hotmail.com>
2024-04-30 17:22:14 -0300

    4ta parte

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,49 +1,41 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  ParseUUIDPipe,
-  Patch,
-  Post,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param,ParseUUIDPipe, Patch, Post} from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
-@UsePipes(ValidationPipe)
 export class CarsController {
-  constructor(private readonly carsService: CarsService) {}
+  
+  constructor(
+    private readonly carsService: CarsService
+  ) {}
 
   @Get()
-  getCars() {
-    return this.carsService.getAll();
+  getAllCars() {
+    return this.carsService.findAll()
   }
 
   @Get(':id')
-  getCarById(@Param('id', ParseUUIDPipe) id: string) {
-    return this.carsService.findCarById(id);
+  getCarById( @Param('id', ParseUUIDPipe ) id: string ) {
+    return this.carsService.findOneById( id );
   }
 
-  @Post(':id')
-  createCar(@Body() createCarDto: CreateCarDto) {
-    return createCarDto;
+  @Post()
+  createCar( @Body() createCardDto: CreateCarDto ) {
+    return this.carsService.create( createCardDto );
   }
 
   @Patch(':id')
-  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
-    return body;
+  updateCar( 
+    @Param('id', ParseUUIDPipe ) id: string, 
+    @Body() updateCarDto: UpdateCarDto ) 
+  {
+    return this.carsService.update( id, updateCarDto );
   }
 
   @Delete(':id')
-  deleteCar(@Param('id', ParseIntPipe) id: number) {
-    return {
-      method: 'delete',
-      id,
-    };
+  deleteCar( @Param('id', ParseUUIDPipe ) id: string ) {
+    return this.carsService.delete( id )
   }
+
 }
--- src/cars/interfaces/car.interface.ts
+++ src/cars/interfaces/car.interface.ts
@@ -1,5 +1,8 @@
+
 export interface Car {
-    id: string,
-    brand: string,
-    model: string
+
+    id:    string;
+    brand: string;
+    model: string;
+
 }
